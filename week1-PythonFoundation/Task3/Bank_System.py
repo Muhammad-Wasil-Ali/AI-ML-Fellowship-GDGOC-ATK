bb897049bd48e1c8ac5963bfe4ff3ea3b4a6e31b
Muhammad Wasil Ali <wasilali4999@gmail.com>
2026-01-29 20:07:23 +0500

    Saving local changings

--- week1-PythonFoundation/Task3/Bank_System.py
+++ week1-PythonFoundation/Task3/Bank_System.py
@@ -1,7 +1,7 @@
 class BankAccount:
     def __init__(self, owner, balance=0):
         self.owner = owner
-        self._balance = balance  # protected
+        self._balance = balance  
 
     def deposit(self, amount):
         if amount > 0:
